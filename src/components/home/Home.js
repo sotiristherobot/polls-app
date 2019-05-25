1ef676b656eed06bd6afafb07d1d18abd27d1356
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-25 06:56:23 +0200

    GET questions from https://polls.apiblueprint.org/questions
    Makes a GET request to endpoint and if succesfull pushes the questions
    to the state of <Home/>. In case of error prints the error to console.
    This can be changed to show an alternative message to end user in the
    future.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
 import Header from "../header/Header";
+import axios from "axios";
 
 // ui-related
 import { Grommet, Box } from "grommet";
 
+// models
+import QuestionModel from "../../models/QuestionModel";
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: []
+    };
+  }
+
+  /**
+   * Fetches questions from endpoint on componentDidMount lifecycle event
+   * @returns {AxiosPromise<any>}
+   */
+  fetchQuestions() {
+    return axios.get("https://polls.apiblueprint.org/questions");
+  }
+
+  /**
+   * React lifecycle event.
+   */
+  componentDidMount() {
+    this.fetchQuestions().then(response => {
+      const { data } = response;
+      this.setState({
+        questions: data.map(
+          item =>
+            new QuestionModel(item.question, item.published_at, item.choices)
+        )
+      });
+    }).catch(e => console.log(e));
+  }
+
   render() {
     return (
       <Grommet full={true}>
