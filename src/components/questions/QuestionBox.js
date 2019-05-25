7d5816b61cf6d9fc37bd6a239c6edc71ee2ad385
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-25 06:45:54 +0200

    Created <QuestionBox/>
    A <QuestionBox/> stateless component is created for every question retrieved
    from the API endpoint. It contains a question, date published and number,
    of choices. Exposees an onClick event, that will direct to the question,
    detail page.

---
+++ src/components/questions/QuestionBox.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Box, Text } from "grommet";
+
+/**
+ * Stateless component that creates a QuestionBox for each question, that contain
+ * a question, date published and number of choices.
+ * @param props
+ */
+export default function QuestionBox(props) {
+  return (
+    <Box
+      direction="column"
+      border="all"
+      width="medium"
+      pad="small"
+      margin="small"
+      onClick={props.onQuestionBoxClick}
+    >
+      <Text>
+        <b>Question:</b> {props._question}
+      </Text>
+      <Text>
+        <b>Published:</b> {props._publishedAt}
+      </Text>
+      <Text>
+        <b>Choices:</b> {props._choices.length}
+      </Text>
+    </Box>
+  );
+}
