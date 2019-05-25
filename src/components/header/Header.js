553f80f2778b64af4061e1d9e490afd9c22bc4bc
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-25 06:35:19 +0200

    Removed <Box/> from <Header/>
    Removed the unecessary extra container from <Header/>

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { Box, Heading } from "grommet";
+import { Heading } from "grommet";
 
 /**
  * Uses component composition to wrap prop children around a <Heading/>
  * @param props
  */
 export default function Header(props) {
-  return (
-    <Box>
-      <Heading margin="none">{props.children}</Heading>
-    </Box>
-  );
+  return <Heading margin="none">{props.children}</Heading>;
 }
