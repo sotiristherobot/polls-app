59351eaa10d13b429b1f2ee16ad214989cef853f
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-25 06:31:47 +0200

    Created QuestionModel, ChoiceModel
    Instances of QuestionModel represent questions pull from a GET request,
    and are pushed into the state of <Home/>
    Instances of ChoiceModel are contained as part of QuestionModel with a
    one-to-many relation.

---
+++ src/models/QuestionModel.js
@@ -0,0 +1,37 @@
+import ChoiceModel from "./ChoiceModel";
+
+export default class QuestionModel {
+  constructor(question, publishedAt, choices) {
+    this._question = question;
+    this._publishedAt = this.convertDate(publishedAt);
+    this._choices = choices.map(
+      (choice, index) => new ChoiceModel(index, choice.choice, choice.vote)
+    );
+    this._id = QuestionModel.countInstances;
+    QuestionModel.countInstances++;
+  }
+
+  /**
+   * @param date
+   * @returns {string} - Date
+   */
+  convertDate(date) {
+    return new Date(date).toDateString();
+  }
+
+  // getters - setters
+  get id() {
+    return this._id;
+  }
+  get question() {
+    return this._question;
+  }
+  get publishedAt() {
+    return this._publishedAt;
+  }
+  get choices() {
+    return this._choices;
+  }
+}
+// static property to count how many instances of this model we have created
+QuestionModel.countInstances = 0;
