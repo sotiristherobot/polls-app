ff1d36cef780e2bbac4f2364c3c2f1cee90fd75d
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-29 12:17:55 +0200

    Created QuestionDetailBox.test.js

---
+++ src/components/questions/QuestionDetailBox.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import QuestionDetailBox from "./QuestionDetailBox";
+import QuestionModel from "../../models/QuestionModel";
+import { shallow } from "enzyme";
+
+const mockedProps = () => {
+  const props = {
+    question: QuestionModel.createModelInstances([
+      {
+        url: "/questions/12331",
+        published_at: "2019-05-27T11:50:56.522266+00:00",
+        question: "Favourite programming rocketman?",
+        choices: [
+          {
+            url: "/questions/12331/choices/50702",
+            votes: 0,
+            choice: "Objective-C"
+          },
+          {
+            url: "/questions/12331/choices/50701",
+            votes: 0,
+            choice: "Python"
+          },
+          { url: "/questions/12331/choices/50703", votes: 0, choice: "Ruby" }
+        ]
+      }
+    ])
+  };
+
+  const actions = {
+    resetActiveQuestion: jest.fn()
+  };
+
+  return {
+    props,
+    actions
+  };
+};
+
+describe("<QuestionDetailBox/>", () => {
+  const { props, actions } = mockedProps();
+  const shallowWrapper = shallow(
+    <QuestionDetailBox
+      question={props.question[0]}
+      resetActiveQuestion={actions.resetActiveQuestion}
+    />
+  );
+
+  it("renders <QuestionDetailBox/> without crashing", () => {
+    expect(shallowWrapper).toHaveLength(1);
+  });
+
+});
