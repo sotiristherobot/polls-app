832819da94f5c1a62abb22b24a4cce0348fdc7dc
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-29 13:28:13 +0200

    Check that <Form/> is rendered with the correct children
    The form should be rendered, with 3 radio buttons, 3 labels that will
    contain the number of votes and a submit button.

--- src/components/questions/QuestionDetailBox.test.js
+++ src/components/questions/QuestionDetailBox.test.js
@@ -56,4 +56,22 @@ describe("<QuestionDetailBox/>", () => {
     expect(inputField.text()).toBe("Question: Favourite programming rocketman?");
   });
 
+  it("should render a <Form/>", () => {
+    const form = shallowWrapper.children("Form");
+
+    expect(form).toHaveLength(1);
+  });
+
+  it("should render a <Form/> with an <RadioButton/>[3] <Text/> and <Button/> ", () => {
+    const form = shallowWrapper.children("Form").render(),
+        radioButtons = form.find('input'),
+        label = form.find('label'),
+        submitButton = form.find('button[type="submit"]');
+
+    expect(radioButtons).toHaveLength(3);
+    expect(submitButton).toHaveLength(1);
+    expect(label).toHaveLength(3);
+    expect(label.first().text()).toBe('Objective-C');
+    expect(label.last().text()).toBe('Ruby');
+  });
 });
